7ab5da617cd38795eaeb89a2ce7e9e6741300423
Alexandr Kuznetsov <kuznetsovshurik@gmail.com>
2019-05-17 19:27:18 +0600

    Add base api controller with mediator methods

---
+++ FileUploadApp.Core/BaseApiController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileUploadApp.Core
+{
+    [ApiController]
+    public abstract class BaseApiController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        protected BaseApiController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        protected async Task PublishAsync(INotification notification, CancellationToken cancellationToken = default)
+        {
+            await mediator.Publish(notification, cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        protected async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            return await mediator.Send(request, cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
