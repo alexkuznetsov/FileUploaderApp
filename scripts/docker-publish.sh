60bab8839547a4161055d0ad96108148609366a5
Alexandr Kuznetsov <kuznetsovshurik@gmail.com>
2019-06-02 15:52:33 +0600

    Added TravisCI support

---
+++ scripts/docker-publish.sh
@@ -0,0 +1,18 @@
+DOCKER_ENV=''
+DOCKER_TAG=''
+
+case "$TRAVIS_BRANCH" in
+  "master")
+    DOCKER_ENV=production
+    DOCKER_TAG=latest
+    ;;
+  "develop")
+    DOCKER_ENV=development
+    DOCKER_TAG=dev
+    ;;    
+esac
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -t fileuploaderapp:$DOCKER_TAG .
+docker tag fileuploaderapp:$DOCKER_TAG $DOCKER_USERNAME/fileuploaderapp:$DOCKER_TAG
+docker push $DOCKER_USERNAME/fileuploaderapp:$DOCKER_TAG
