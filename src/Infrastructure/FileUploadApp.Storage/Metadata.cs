c280b35a19c717339ca2a7abe54d3760dd0c4775
Kuznetsov Alexander <kuznetsov@dm.findexit.kz>
2024-06-05 15:37:12 +0500

    fix bug with serialize model

--- src/Infrastructure/FileUploadApp.Storage/Metadata.cs
+++ src/Infrastructure/FileUploadApp.Storage/Metadata.cs
@@ -2,9 +2,27 @@
 
 namespace FileUploadApp.Storage;
 
-public record Metadata(Guid Id, string Name, string ContentType, DateTime CreatedDate)
+public class Metadata
 {
+    public Guid Id { get; set; } = Guid.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; } = DateTime.MinValue;
+
+    public Metadata()
+    {
+
+    }
+
     public Metadata(Guid id, string name, string contentType, DateTime? dateTime = null) : this(id, name, contentType, dateTime ?? DateTime.UtcNow)
     {
+    }
+
+    public Metadata(Guid id, string name, string contentType, DateTime dateTime)
+    {
+        Id = id;
+        Name = name;
+        ContentType = contentType;
+        CreatedDate = dateTime;
     }
 }
